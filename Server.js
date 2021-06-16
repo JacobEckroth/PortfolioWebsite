9f6d15763275f21b5c546492be7495446ce8f2d2
JacobEckroth <jacobeckroth@gmail.com>
2021-06-15 19:30:59 -0700

    inital stuff

---
+++ Server.js
@@ -0,0 +1,37 @@
+const exphbs = require('express-handlebars')
+const express = require('express')
+const fs = require('fs');
+const app = express()
+const path = require('path')
+
+const config = require('./config.json')
+const port = config.runPort;
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('portfolioHomePage')
+})
+
+app.listen(port, () => {
+    app.set('views', __dirname + '/views');
+    app.engine('handlebars', exphbs({
+        defaultLayout: 'main',
+        extname: 'handlebars',
+        layoutsDir: path.join(__dirname, "/views/layouts"),
+        partialsDir: [
+            path.join(__dirname, "/views/partials")
+        ],
+        helpers: {
+            section: function (name, options) {
+                if (!this._sections) this._sections = {}
+                this._sections[name] = options.fn(this)
+                return null
+            },
+        }
+    }));
+    app.set('view engine', 'handlebars');
+    app.use(express.static(__dirname + '/public'));
+    console.log(`Example app listening at http://localhost:${port}`)
+})
